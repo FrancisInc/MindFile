f94f8e5078b6b6709df2fe412c1084c273ef683a
FrancisInc <30735274+FrancisInc@users.noreply.github.com>
2017-08-04 23:25:07 +0100

    Added source files

---
+++ MindFileClassLibrary/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MindFileClassLibrary
+{
+    class User
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<string> Hobbies { get; set; }
+        public string FavouriteSong { get; set; }
+    }
+}
