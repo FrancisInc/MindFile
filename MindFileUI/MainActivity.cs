fcd92091a5008dac9c0ef644544b7f63e8b8135f
FrancisInc <30735274+FrancisInc@users.noreply.github.com>
2017-08-04 23:13:29 +0100

    Added source files

--- MindFileUI/MainActivity.cs
+++ MindFileUI/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace MindFileUI
 {
-    [Activity(Label = "MindFileUI", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Mind File", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
